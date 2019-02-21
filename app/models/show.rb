86ee574ddf1dd36443b786277f7547414ee5c285
Robert McCary <robert.mccary@yahoo.com>
2019-02-21 00:28:15 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -15,4 +15,16 @@ class Show < ActiveRecord::Base
   def self.least_popular_show
     self.where("rating = ?", self.lowest_rating).first
   end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end 
+
+  def self.popular_shows
+    self.where("rating > 5")
+  end 
+
+  def self.shows_by_alphabetical_order
+    Show.sort(:name)
+  end  
 end
